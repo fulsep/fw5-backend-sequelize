f792e2896b9db7ca45be1f6dc8e0e1667eaedc95
HirasParasian <hirasparasian@gmail.com>
2022-03-22 13:21:32 +0700

    Feat : Product Images

--- src/models/productImages.js
+++ src/models/productImages.js
@@ -4,13 +4,13 @@ const sequelize = require("../helpers/sequelize")
 const Product = require("./product")
 
 const ProductImage = sequelize.define("product_images", {
-  // productId: {
-  //   type: Sequelize.INTEGER,
-  //   references: {
-  //     model: Product,
-  //     key: "id"
-  //   }
-  // },
+  productId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Product,
+      key: "id"
+    }
+  },
   imagesProduct: Sequelize.STRING
 })
 
