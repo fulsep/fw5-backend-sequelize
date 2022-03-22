8c97b955b17a6be4b8902b053be8fe8d3c253e4a
DanangFirmantara <danangfirmantara837@gmail.com>
2022-03-22 13:51:49 +0700

    fix : error

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,5 @@ const routes = require("express").Router()
 
 routes.use("/users", require("./users"))
 routes.use("/comments", require("./comments"))
-routes.use("/phones", require("./phones"))
 
 module.exports = routes
